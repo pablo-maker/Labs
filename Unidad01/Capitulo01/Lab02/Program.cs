4f1db1717a5ae9cda8faedea20a16aafe34064a6
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-03-30 15:25:54 -0300

    Add WriteLine

--- Unidad01/Capitulo01/Lab02/Program.cs
+++ Unidad01/Capitulo01/Lab02/Program.cs
@@ -12,7 +12,13 @@ namespace Lab02
         {
             string valor1 = "Este es el valor 1";
             int valor2 = 5;
-            string valor3 = valor2;
+            string valor3 = valor1;
+            Console.WriteLine(valor1);
+            Console.WriteLine(valor2);
+            Console.WriteLine(valor3);
+            Console.WriteLine();
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
         }
     }
 }

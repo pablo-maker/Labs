91bc9b911be12e71e18e79b105af63ddab5095c8
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-04-29 12:29:44 -0300

    add for to read and show array

--- Unidad02/Capitulo01/LabSintaxis3/Program.cs
+++ Unidad02/Capitulo01/LabSintaxis3/Program.cs
@@ -10,6 +10,22 @@ namespace LabSintaxis3
     {
         static void Main(string[] args)
         {
+            string[] arregloStrings; //Definicion de un Arreglo de strings
+            arregloStrings = new string[5]; //De 5 elementos
+
+            int cantIteraciones = 5;
+
+            Console.WriteLine("Ingresar texto:");
+
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                Console.WriteLine("\n"+(i+1)+" de "+cantIteraciones);
+                arregloStrings[i] = Console.ReadLine();
+            }
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine("\n" + arregloStrings[i]);
+            }
         }
     }
 }

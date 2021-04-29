6c9f27dbaa5a55b506020d018203b1f3f10df3b7
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-04-29 15:34:59 -0300

    Solve 4.8

---
+++ Unidad02/Capitulo01/LabSintaxis4.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 0;
+            bool claveEsCorrecta = false;
+            string clave = "clave";
+
+            while (intentos < 4 && !claveEsCorrecta)
+            {
+                Console.WriteLine("Ingrese la clave:");
+                string contra = Console.ReadLine();
+                if (contra == clave)
+                {
+                    Console.WriteLine("Clave correcta.");
+                    claveEsCorrecta = true;
+                }
+                else
+                {
+                    intentos++;
+                }
+            }
+            
+
+        }
+    }
+}

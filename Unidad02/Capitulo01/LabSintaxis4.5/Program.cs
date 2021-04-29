5679242f303446376d9b61b8496efec672d7b4be
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-04-29 14:46:44 -0300

    Solve 4.5

---
+++ Unidad02/Capitulo01/LabSintaxis4.5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meses = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+            Console.WriteLine("Ingrese nombre del Mes:");
+            string mes = Console.ReadLine();
+            for (int i=0; i< meses.Length; i++)
+            {
+                if (mes == meses[i])
+                    Console.WriteLine("El numero del mes " + mes + " es " + (i + 1));
+            }
+        }
+    }
+}

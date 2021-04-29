3451a4731d8f664e53b8f95b90c35bd7c45fd28d
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-04-29 12:05:22 -0300

    Add menu

--- Unidad02/Capitulo01/LabSintaxis2/Program.cs
+++ Unidad02/Capitulo01/LabSintaxis2/Program.cs
@@ -20,6 +20,28 @@ namespace LabSintaxis2
             {
                 Console.WriteLine("Error, no se ingreso ningun texto");
             }
+            //Menu
+            Console.Clear();
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1) Mostrar la frase ingresada en MAYUSCULAS");
+            Console.WriteLine("2) Mostrar la frase ingresada en minusculas");
+            Console.WriteLine("3) Contar cantidad de caracteres");
+            Console.Write("\r\nSeleccionar una opcion: ");
+
+            ConsoleKeyInfo opcion = Console.ReadKey();
+            Console.Write("\r\n");
+            switch (opcion.Key.ToString())
+            {
+                case "D1":
+                    Console.WriteLine(inputTexto.ToUpper());
+                    break;
+                case "D2":
+                    Console.WriteLine(inputTexto.ToLower());
+                    break;
+                case "D3":
+                    Console.WriteLine(inputTexto.Length);
+                    break;
+            }
         }
     }
 }

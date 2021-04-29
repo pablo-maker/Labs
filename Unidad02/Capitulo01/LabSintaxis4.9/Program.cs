5bb3520a98d41954864a9dff746e73eb95f18168
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-04-29 15:44:10 -0300

    Solve 4.9

---
+++ Unidad02/Capitulo01/LabSintaxis4.9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSintaxis4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese numero de pisos");
+            int n = Convert.ToInt32(Console.ReadLine());
+            n++;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j <= n - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.Write("\n");
+            }
+        }
+    }
+}

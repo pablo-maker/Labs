b6874cb7837eb7b67e368b40ef6a5b8e3828b300
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-06 12:45:49 -0300

    add class B

---
+++ Unidad02/Capitulo02/Clases/B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class B : A
+    {
+        public B() : base("Instancia de B")
+        {
+
+        }
+        public string M4()
+        {
+            return "Metodo del hijo invocado";
+        }
+    }
+}

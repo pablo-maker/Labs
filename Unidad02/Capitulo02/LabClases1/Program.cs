1a6467ed7301dab21dddba8d76cc3990f802da35
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-06 15:01:42 -0300

    add program.cs

--- Unidad02/Capitulo02/LabClases1/Program.cs
+++ Unidad02/Capitulo02/LabClases1/Program.cs
@@ -14,7 +14,19 @@ namespace LabClases1
         {
             A claseA = new A();
             claseA.MostrarNombre();
+            claseA.M1();
+            claseA.M2();
+            claseA.M3();
 
+            A claseA2 = new A("Test");
+            claseA2.MostrarNombre();
+
+            B claseB = new B();
+            claseB.MostrarNombre();
+            claseB.M1();
+            claseB.M2();
+            claseB.M3();
+            claseB.M4();
         }
     }
 }

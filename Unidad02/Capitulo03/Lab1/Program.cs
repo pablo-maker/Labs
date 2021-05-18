873a8f3c1479fcc1f90610e443156aa1e557b625
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-18 13:37:54 -0300

    finish lab1

--- Unidad02/Capitulo03/Lab1/Program.cs
+++ Unidad02/Capitulo03/Lab1/Program.cs
@@ -10,6 +10,12 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            string[] provincias = { "Buenos Aires", "Buenos Aires Capital", "Catamarca", "Chaco", "Chubut", "Cordoba", "Corrientes", "Entre Rios", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquen", "Rio Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucuman" };
+            var prov = from p in provincias
+                       where p.StartsWith("S") || p.StartsWith("T")
+                       select p;
+            Console.WriteLine("Las provincias que empiezan con S o T son: "+string.Join(", ", prov));
+
         }
     }
 }

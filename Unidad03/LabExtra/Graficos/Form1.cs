53063f27e0bddda4948888be93d2a1dca2bbe312
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-28 08:58:48 -0300

    More serious

--- Unidad03/LabExtra/Graficos/Form1.cs
+++ Unidad03/LabExtra/Graficos/Form1.cs
@@ -20,16 +20,55 @@ namespace Graficos
         {
             // Get the graphics object 
             Graphics gfx = paintEvnt.Graphics;
-            // Create a new pen that we shall use for drawing the line 
-            Pen myPen = new Pen(Color.Black);
+            int x1 = 0;
+            int y1 = 0;
 
-            // Loop until the coordinates reach 250 (the lower right corner of the form) 
-            for (int i = 0; i < 250; i = i + 50)
+            // Loop trough the 255 values red can have 
+            for (int i = 0; i <= 255; i++)
             {
-                // Draw a 50x50 pixels rectangle 
-                gfx.DrawRectangle(myPen, i, i, 50, 50);
+                // Create new brush with a defined color 
+                Color brushColor = Color.FromArgb(i, 0, 0);
+                // The brush is solid because we want a solid rectangle 
+                SolidBrush myBrush = new SolidBrush(brushColor);
+                // Actually draw the rectangle 
+                gfx.FillRectangle(myBrush, x1, y1, 10, 10);
+                // The next rectangle should be near the last one 
+                x1 = x1 + 10;
+                // If the row is complete start another one 
+                if ((x1 % 290) == 0)
+                {
+                    y1 = y1 + 10;
+                    x1 = 0;
+                }
+            }
+
+            for (int i = 0; i <= 255; i++)
+            {
+                Color brushColor = Color.FromArgb(0, i, 0);
+                SolidBrush myBrush = new SolidBrush(brushColor);
+                gfx.FillRectangle(myBrush, x1, y1, 10, 10);
+                x1 = x1 + 10;
+                if ((x1 % 290) == 0)
+                {
+                    y1 = y1 + 10;
+                    x1 = 0;
+                }
+            }
+
+            for (int i = 0; i <= 255; i++)
+            {
+                Color brushColor = Color.FromArgb(0, 0, i);
+                SolidBrush myBrush = new SolidBrush(brushColor);
+                gfx.FillRectangle(myBrush, x1, y1, 10, 10);
+                x1 = x1 + 10;
+                if ((x1 % 290) == 0)
+                {
+                    y1 = y1 + 10;
+                    x1 = 0;
+                }
             }
         }
+
 
 
 

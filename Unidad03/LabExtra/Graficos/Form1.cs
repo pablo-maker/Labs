76645935d1ca61c5963199cd3972736d8ee6ea74
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-28 08:56:45 -0300

    squares

--- Unidad03/LabExtra/Graficos/Form1.cs
+++ Unidad03/LabExtra/Graficos/Form1.cs
@@ -16,5 +16,24 @@ namespace Graficos
         {
             InitializeComponent();
         }
+        protected override void OnPaint(PaintEventArgs paintEvnt)
+        {
+            // Get the graphics object 
+            Graphics gfx = paintEvnt.Graphics;
+            // Create a new pen that we shall use for drawing the line 
+            Pen myPen = new Pen(Color.Black);
+            // Loop and create a horizontal line 10 pixels below the last one 
+            for (int i = 20; i <= 250; i = i + 10)
+            {
+                gfx.DrawLine(myPen, 20, i, 270, i);
+            }
+            // Loop and create a vertical line 10 pixels next to the last one 
+            for (int x = 20; x < 280; x = x + 10)
+            {
+                gfx.DrawLine(myPen, x, 20, x, 250);
+            }
+        }
+
+
     }
 }

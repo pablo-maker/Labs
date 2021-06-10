81624268f061ee1496a8ea3ac0d1a531f81d5f68
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-10 13:19:37 -0300

    Update Program.cs

--- Unidad04/Lab01/Program.cs
+++ Unidad04/Lab01/Program.cs
@@ -19,12 +19,14 @@ namespace Lab01
             */
             StreamReader lector = File.OpenText("agenda.txt");
             string linea;
+            Console.WriteLine("Nombre\tApellido\te-mail\t\t\tTelefono");
             do
             {
                 linea = lector.ReadLine();
                 if (linea != null)
                 {
-                    Console.WriteLine(linea);
+                    string[] valores = linea.Split(';');
+                    Console.WriteLine("{0}\t{1}\t{2}\t{3}",valores[0], valores[1], valores[2], valores[3]);
                 }
             } while (linea != null);
             lector.Close();

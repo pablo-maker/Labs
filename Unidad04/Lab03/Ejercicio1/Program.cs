ecf0a535a371e657aee99e0090f1a451233b8a86
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-18 19:47:49 -0300

    Finish ej. 1

--- Unidad04/Lab03/Ejercicio1/Program.cs
+++ Unidad04/Lab03/Ejercicio1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Ejercicio1
 {
@@ -10,6 +12,28 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            DataTable dtAlumnos = new DataTable();
+            DataRow rwAlumno = dtAlumnos.NewRow();
+
+            dtAlumnos.Columns.Add("Apellido");
+            dtAlumnos.Columns.Add("Nombre");
+
+            rwAlumno["Apellido"] = "Perez";
+            rwAlumno["Nombre"] = "Marcelo";
+
+            dtAlumnos.Rows.Add(rwAlumno);
+
+            DataRow rwAlumno2 = dtAlumnos.NewRow();
+            rwAlumno2["Apellido"] = "Perez";
+            rwAlumno2["Nombre"] = "Juan";
+
+            dtAlumnos.Rows.Add(rwAlumno2);
+
+            Console.WriteLine("Listado de Alumnos:");
+            foreach (DataRow row in dtAlumnos.Rows)
+            {
+                Console.WriteLine(row["Apellido"].ToString() + "," + row["Nombre"].ToString());
+            }
         }
     }
 }

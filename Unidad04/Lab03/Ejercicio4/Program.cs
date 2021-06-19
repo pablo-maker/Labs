b835d6bb99721289a863d7f95ba070891afbb1ac
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-18 23:26:49 -0300

    Finish EJ.6

--- Unidad04/Lab03/Ejercicio4/Program.cs
+++ Unidad04/Lab03/Ejercicio4/Program.cs
@@ -58,6 +58,50 @@ namespace Ejercicio4
 
             }
             Console.ReadKey();
+
+
+            //Indico el CustomerId que deseo modificar
+            Console.WriteLine("Escriba el CustomerID que desea modificar: ");
+            string custId = Console.ReadLine();
+
+            //Me traigo una coleccion de dataRows que contengan ese customerid
+            DataRow[] rwEmpresas = dtEmpresas.Select("CustomerID = '" + custId + "'");
+            if (rwEmpresas.Length != 1)
+            {
+                Console.WriteLine("CustomerID no encontrado");
+                Console.ReadLine();
+                return;
+            }
+            //Primer datarow de la coleccion
+            DataRow rowMiEmpresa = rwEmpresas[0];
+            string nombreActual = rowMiEmpresa["CompanyName"].ToString();
+            Console.WriteLine("Nombre actual de la empresa: " + nombreActual);
+            Console.WriteLine("Escriba el nuevo nombre: ");
+            string nuevoNombre = Console.ReadLine();
+
+            //Metodo beginedit del datarow 
+            rowMiEmpresa.BeginEdit();
+            //Modifico el valor del campo
+            rowMiEmpresa["CompanyName"] = nuevoNombre;
+            //Se finaliza llamando al metodo EndEdit()
+            rowMiEmpresa.EndEdit();
+
+            //Se crea un objeto Command para guardar los cambios
+            SqlCommand updCommand = new SqlCommand();
+            updCommand.Connection = myconn;
+            //Indico la cadena TSQL
+            updCommand.CommandText =
+                "UPDATE Customers SET CompanyName = @CompanyName WHERE CustomerID = @CustomerID";
+            //Indico los parametros que estoy utilizando, el tipo de dato, la longitud y el nombre del campo del datatable
+            updCommand.Parameters.Add("@CompanyName", SqlDbType.NVarChar, 50, "CompanyName");
+            updCommand.Parameters.Add("@CustomerID", SqlDbType.NVarChar, 5, "CustomerID");
+
+            //Adjunto el objeto updcommand al dataAdapter
+            myAdap.UpdateCommand = updCommand;
+            //Luego llamamos al metodo update
+            myAdap.Update(dtEmpresas);
+
+
         }
     }
 }

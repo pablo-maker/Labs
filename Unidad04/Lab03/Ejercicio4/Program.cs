47896db8ce4ea3b22faebe3a995d596447c28992
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-18 22:44:25 -0300

    Add Ej4

---
+++ Unidad04/Lab03/Ejercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTable dtEmpresas = new DataTable("Empresas");
+
+            dtEmpresas.Columns.Add("CustomerID", typeof(string));
+        }
+    }
+}

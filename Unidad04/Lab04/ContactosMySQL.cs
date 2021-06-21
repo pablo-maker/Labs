434000b46f62a9edba87ef890c472185038b9698
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-21 13:28:23 -0300

    Update ContactosMySQL.cs

--- Unidad04/Lab04/ContactosMySQL.cs
+++ Unidad04/Lab04/ContactosMySQL.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace Lab04
 {
-    class Class1
+    class ContactosMySQL : Contactos
     {
+        protected string connectionString
+        {
+            get
+            {
+                return "server=localhost;database=net;uid=root;";
+            }
+        }
+        public override DataTable getTabla()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmdSelect = new MySqlCommand("SELECT * FROM contactos", conn);
+                conn.Open();
+                MySqlDataReader reader = cmdSelect.ExecuteReader();
+                DataTable contactos = new DataTable();
+                if (reader != null)
+                {
+                    contactos.Load(reader);
+                }
+                conn.Close();
+                return contactos;
+            }
+        }
     }
 }

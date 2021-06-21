9680b932f9954d1a35864de09a83636163831023
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-21 13:36:06 -0300

    up to 31)

--- Unidad04/Lab04/ContactosMySQL.cs
+++ Unidad04/Lab04/ContactosMySQL.cs
@@ -33,5 +33,71 @@ namespace Lab04
                 return contactos;
             }
         }
+        public override void aplicarCambios()
+        {
+            using (MySqlConnection Conn = new MySqlConnection(connectionString))
+            {
+                MySqlCommand cmdInsert = new MySqlCommand("INSERT INTO contactos VALUES(@id,@nombre,@apellido,@email,@telefono)", Conn);
+                cmdInsert.Parameters.Add("@id", MySqlDbType.Int32);
+                cmdInsert.Parameters.Add("@nombre", MySqlDbType.VarChar);
+                cmdInsert.Parameters.Add("@apellido", MySqlDbType.VarChar);
+                cmdInsert.Parameters.Add("@email", MySqlDbType.VarChar);
+                cmdInsert.Parameters.Add("@telefono", MySqlDbType.VarChar);
+
+                MySqlCommand cmdUpdate = new MySqlCommand(
+                "UPDATE contactos SET nombre=@nombre, apellido=@apellido,email=@email,telefono=@telefono where id=@id", Conn);
+                cmdUpdate.Parameters.Add("@id", MySqlDbType.Int32);
+                cmdUpdate.Parameters.Add("@nombre", MySqlDbType.VarChar);
+                cmdUpdate.Parameters.Add("@apellido", MySqlDbType.VarChar);
+                cmdUpdate.Parameters.Add("@email", MySqlDbType.VarChar);
+                cmdUpdate.Parameters.Add("@telefono", MySqlDbType.VarChar);
+
+                MySqlCommand cmdDelete = new MySqlCommand("DELETE FROM contactos WHERE id=@id", Conn);
+                cmdDelete.Parameters.Add("@id", MySqlDbType.Int32);
+
+                DataTable filasNuevas = this.misContactos.GetChanges(DataRowState.Added);//Buscamos filas que fueron agregadas
+                DataTable filasBorradas = this.misContactos.GetChanges(DataRowState.Deleted);//Buscamos filas que fueron borradas
+                DataTable filasModificadas = this.misContactos.GetChanges(DataRowState.Modified);//Buscamos filas que fueron modificadas
+
+                Conn.Open();
+
+                if (filasNuevas != null)
+                {
+                    foreach (DataRow fila in filasNuevas.Rows)
+                    {
+                        cmdInsert.Parameters["@id"].Value = fila["id"];
+                        cmdInsert.Parameters["@nombre"].Value = fila["nombre"];
+                        cmdInsert.Parameters["@apellido"].Value = fila["apellido"];
+                        cmdInsert.Parameters["@email"].Value = fila["email"];
+                        cmdInsert.Parameters["@telefono"].Value = fila["telefono"];
+                        cmdInsert.ExecuteNonQuery();
+                    }
+                }
+                if (filasBorradas != null)
+                {
+                    foreach (DataRow fila in filasBorradas.Rows)
+                    {
+                        cmdDelete.Parameters["@id"].Value = fila["id", DataRowVersion.Original];
+                        cmdDelete.ExecuteNonQuery();
+                    }
+                }
+                if (filasModificadas != null)
+                {
+                    foreach (DataRow fila in filasModificadas.Rows)
+                    {
+                        cmdUpdate.Parameters["@id"].Value = fila["id"];
+                        cmdUpdate.Parameters["@nombre"].Value = fila["nombre"];
+                        cmdUpdate.Parameters["@apellido"].Value = fila["apellido"];
+                        cmdUpdate.Parameters["@email"].Value = fila["email"];
+                        cmdUpdate.Parameters["@telefono"].Value = fila["telefono"];
+                        cmdUpdate.ExecuteNonQuery();
+                    }
+                }
+
+                Conn.Close();
+                this.misContactos.AcceptChanges();
+            }
+        }
+
     }
 }
--- Unidad04/Lab04/Program.cs
+++ Unidad04/Lab04/Program.cs
@@ -10,6 +10,46 @@ namespace Lab04
     {
         static void Main(string[] args)
         {
+            Contactos contactos = new ContactosMySQL();
+            contactos.listar();
+            menu(contactos);
+
         }
+
+        static void menu(Contactos contactos)
+        {
+            string rta = "";
+            do
+            {
+                Console.WriteLine("1 - Listar");
+                Console.WriteLine("2 - Agregar");
+                Console.WriteLine("3 - Modificar");
+                Console.WriteLine("4 - Eliminar");
+                Console.WriteLine("5 - Guardar Cambios");
+                Console.WriteLine("6 - Salir");
+                rta = Console.ReadLine();
+                switch (rta)
+                {
+                    case "1":
+                        contactos.listar();
+                        break;
+                    case "2":
+                        contactos.nuevaFila();
+                        break;
+                    case "3":
+                        contactos.editarFila();
+                        break;
+                    case "4":
+                        contactos.eliminarFila();
+                        break;
+                    case "5":
+                        contactos.aplicarCambios();
+                        break;
+                    default:
+                        break;
+                }
+            } while (rta != "6");
+        }
+
     }
 }
